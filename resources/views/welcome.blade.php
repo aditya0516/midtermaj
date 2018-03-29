5a8c6b48bb94734671a23b4e9ae89fdfb72f3a31
aditya <avj22@njit.edu>
2018-03-29 02:56:30 -0400

    Commit 7 -  Create a controller, route, and view to show a contact form and add it to the main menu of your site.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Mid-Term Test</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -71,6 +71,7 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
+                        <a href="{{route('contact')}}">Contact</a>
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
                     @endauth
@@ -79,15 +80,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mid-Term Test
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel.com/docs">Made By Aditya Jadhav</a>
+                    <!--<a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://forge.laravel.com">Heroku</a>-->
+                    <a href="https://github.com/aditya0516/midtermaj">GitHub Link</a>
                 </div>
             </div>
         </div>
